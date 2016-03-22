0584ad6644612633f9cfa41c314b52b0b3d389cb
paslo22 <paslo22@gmail.com>
2016-03-22 01:28:07 -0300

    Ahora funciona definitivamente el reproductor de audio

--- web/models.py
+++ web/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 import re
 from django.db import models
+from django.contrib.staticfiles.templatetags.staticfiles import static
 
 class Biografia(models.Model):
 	name = models.CharField('Nombre',max_length=70)
@@ -9,6 +10,10 @@ class Biografia(models.Model):
 	def __str__(self):
 		return self.name
 
+	def formatHelper(self,matchobj):
+		url = static('web/archive/'+matchobj.group('path'))
+		return r'<audio controls><source src="'+url+'" type="audio/mpeg">Su explorador es antiguo\. Actualicelo para reproducir audios\.</audio>'
+
 	def htmlText(self,txt):
 		texto = '<p>'
 		texto+= txt.replace('\n', '</p><p>')
@@ -16,7 +21,7 @@ class Biografia(models.Model):
 		texto = texto.replace('<p>\r</p>','')
 		texto = texto.replace('<p>IM\xc1GENES\r</p>','<h3>Imagenes</h3>')
 		texto = texto.replace('<p>MUSICAS\r</p>','<h3>Musicas</h3>')
-		texto = re.sub(r'<p>\[audio:(?P<path>[a-zA-Z0-9/\.]+)\|titles=[a-zA-Z0-9]+\]\r</p>',r'<audio controls><source src="\1" type="audio/mpeg">Su explorador es antiguo\. Actualicelo para reproducir audios\.</audio>',texto)
+		texto = re.sub(r'<p>\[audio:(?P<path>[a-zA-Z0-9/\.]+)\|titles=[a-zA-Z0-9]+\]\r</p>',self.formatHelper,texto)
 		return texto
 
 	def save(self, *args, **kwargs):
