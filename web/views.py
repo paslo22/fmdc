fbdcd847b924a0870d368ad5210b3d122a57d8d2
manuel14 <manuelzubieta14@gmail.com>
2016-03-24 15:05:45 -0300

    modificado views para contact

--- web/views.py
+++ web/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views import generic
 from datetime import datetime
+from django.template.loader import get_template
+from django.core.mail import EmailMessage
+from django.template import Context
 from .models import Biografia, Efemeride
 
 def index(request):
@@ -22,9 +25,47 @@ def quienesSomos(request):
 def objetivos(request):
 	return render(request, 'web/objetivos.html')
 
-def contacto(request):
-	return render(request, 'web/contacto.html')
+def contact(request):
+    form_class = ContactForm
 
+    # new logic!
+    if request.method == 'POST':
+        form = form_class(data=request.POST)
+
+        if form.is_valid():
+            contact_name = request.POST.get(
+                'contact_name'
+            , '')
+            contact_email = request.POST.get(
+                'contact_email'
+            , '')
+            form_content = request.POST.get('content', '')
+
+            # Email the profile with the 
+            # contact information
+            template = 
+                get_template('contact_template.txt')
+            context = Context({
+                'contact_name': contact_name,
+                'contact_email': contact_email,
+                'form_content': form_content,
+            })
+            content = template.render(context)
+
+            email = EmailMessage(
+                "New contact form submission",
+                content,
+                "Your website" +'',
+                ['youremail@gmail.com'],
+                headers = {'Reply-To': contact_email }
+            )
+            email.send()
+            return redirect('contact')
+
+    return render(request, 'contact.html', {
+        'form': form_class,
+    })
+		
 def efemerides(request):
 	todayDay = datetime.today().day
 	todayMonth = datetime.today().month
