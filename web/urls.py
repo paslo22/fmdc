4cfd788faa938ec2074b93bc208c07be174946b2
paslo22 <paslo22@gmail.com>
2016-04-03 23:14:11 -0300

    Cambios en la visual parte1

--- web/urls.py
+++ web/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^biografias/$', views.BiografiaView.as_view(), name='biografias'),
-	url(ur'^biografias/(?P<filtro>[a-zA-Z ]+/)?$', views.BiografiaView.as_view(), name='biografias'),
+	url(ur'^biografias/(?P<filtro>[\w ]+/)?$', views.BiografiaView.as_view(), name='biografias'),
 	url(r'^biografia/(?P<pk>[0-9]+)/$', views.BiografiaDetailView.as_view(), name='biografia'),
 	url(r'^quienesSomos/$', views.quienesSomos, name='quienesSomos'),
 	url(r'^objetivos/$', views.objetivos, name='objetivos'),
@@ -12,7 +12,7 @@ urlpatterns = [
 	url(r'^construccion/$', views.construccion, name='construccion'),
 	url(r'^efemerides/$', views.efemerides, name='efemerides'),
 	url(r'^benefactores/$', views.benefactores, name='benefactores'),
-	url(ur'^discotecas/(?P<filtro>[a-zA-Z ]+/)?$', views.DiscotecaView.as_view(), name='discotecas'),
+	url(ur'^discotecas/(?P<filtro>[\w ]+/)?$', views.DiscotecaView.as_view(), name='discotecas'),
 	url(r'^discoteca/(?P<pk>[0-9]+)/$', views.DiscotecaDetailView.as_view(), name='discoteca'),
 	url(r'^galeria/$',views.GaleriaView.as_view(), name='galeria'),
 	url(ur'^galeria/(?P<filtro>[\w ]+/)?$', views.GaleriaView.as_view(), name='galeria'),
