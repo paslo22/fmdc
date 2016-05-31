14ab362dfd4681a88a55a186892702a8b316d3ec
manuel14 <manuelzubieta14@gmail.com>
2016-05-31 11:10:46 -0300

    modificado correo para recibir mails

--- fmdc/settings.py
+++ fmdc/settings.py
@@ -135,8 +135,8 @@ MEDIA_URL = '/media/'
 
 #EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER  = 'manuelzubieta14@gmail.com'
-EMAIL_HOST_PASSWORD = 'cambiro22'
+EMAIL_HOST_USER  = 'pedrozubieta@gmail.com'
+EMAIL_HOST_PASSWORD = 'JuPe3420'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 
