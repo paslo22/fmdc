4da4f7b25212e0d414253dc10c9987d4d29060e8
Manuel Zubieta <mzubieta@onalap1175.corp.onapsis.com>
2019-09-11 21:12:09 -0300

    Changing Biografia text to htmlfield

--- web/admin.py
+++ web/admin.py
@@ -3,85 +3,98 @@ import nested_admin
 
 from .models import *
 
+
 class ImageInline(admin.StackedInline):
-	model = Image
-	extra = 1
-	classes = ('grp-collapse grp-open',)
-	inline_classes = ('grp-collapse grp-open',)
+    model = Image
+    extra = 1
+    classes = ('grp-collapse grp-open',)
+    inline_classes = ('grp-collapse grp-open',)
+
 
 class SongInline(admin.StackedInline):
-	model = Song
-	extra = 1
-	exclude = ('link',)
-	classes = ('grp-collapse grp-open',)
-	inline_classes = ('grp-collapse grp-open',)
+    model = Song
+    extra = 1
+    exclude = ('link',)
+    classes = ('grp-collapse grp-open',)
+    inline_classes = ('grp-collapse grp-open',)
+
 
 class BiografiaAdmin(admin.ModelAdmin):
-	inlines = [
-		SongInline,
-		ImageInline,
-	]
-	exclude = ('text',)
-	search_fields=('name__name',)
-	raw_id_fields=('name',)
+    inlines = [
+        SongInline,
+        ImageInline,
+    ]
+    search_fields = ('name__name',)
+    raw_id_fields = ('name',)
 
-admin.site.register(Biografia,BiografiaAdmin)
+
+admin.site.register(Biografia, BiografiaAdmin)
+
 
 class EfemerideMesAdmin(admin.ModelAdmin):
-	exclude = ('monthNumber','texto')
+    exclude = ('monthNumber', 'texto')
 
-admin.site.register(EfemerideMes,EfemerideMesAdmin)
+
+admin.site.register(EfemerideMes, EfemerideMesAdmin)
+
 
 class ArtistaAdmin(admin.ModelAdmin):
-	search_fields=('name',)
+    search_fields = ('name',)
 
-admin.site.register(Artista,ArtistaAdmin)
+
+admin.site.register(Artista, ArtistaAdmin)
+
 
 class ImageAInline(nested_admin.NestedStackedInline):
-	model = ImageAlbum
-	extra = 1
-	classes = ('grp-collapse grp-open',)
-	inline_classes = ('grp-collapse grp-open',)
+    model = ImageAlbum
+    extra = 1
+    classes = ('grp-collapse grp-open',)
+    inline_classes = ('grp-collapse grp-open',)
+
 
 class AlbumInline(nested_admin.NestedStackedInline):
-	inlines = [
-		ImageAInline,
-	]
-	model = Album
-	extra = 3
-	exclude = ('songString',)
-	classes = ('grp-collapse grp-open',)
-	inline_classes = ('grp-collapse grp-open',)
+    inlines = [
+        ImageAInline,
+    ]
+    model = Album
+    extra = 3
+    exclude = ('songString',)
+    classes = ('grp-collapse grp-open',)
+    inline_classes = ('grp-collapse grp-open',)
+
 
 class VideoInline(nested_admin.NestedStackedInline):
-	model = Video
-	extra = 1
-	classes = ('grp-collapse grp-open',)
-	inline_classes = ('grp-collapse grp-open',)	
+    model = Video
+    extra = 1
+    classes = ('grp-collapse grp-open',)
+    inline_classes = ('grp-collapse grp-open',)
+
 
 class ActividadImageInline(nested_admin.NestedStackedInline):
-	model = ActividadImage
-	extra = 1
-	classes = ('grp-collapse grp-open',)
-	inline_classes = ('grp-collapse grp-open',)
+    model = ActividadImage
+    extra = 1
+    classes = ('grp-collapse grp-open',)
+    inline_classes = ('grp-collapse grp-open',)
+
 
 class DiscotecaAdmin(nested_admin.NestedModelAdmin):
-	inlines = [
-		AlbumInline,
-	]
-	search_fields=('name__name',)
-	raw_id_fields=('name',)
+    inlines = [
+        AlbumInline,
+    ]
+    search_fields = ('name__name',)
+    raw_id_fields = ('name',)
 
 
-admin.site.register(Discoteca,DiscotecaAdmin)
+admin.site.register(Discoteca, DiscotecaAdmin)
+
 
 class ActividadAdmin(nested_admin.NestedModelAdmin):
-	inlines = [
-		VideoInline,
-		ActividadImageInline,
-	]
-	exclude = ('text',)
-	search_fields=('title__title',)
-	
+    inlines = [
+        VideoInline,
+        ActividadImageInline,
+    ]
+    exclude = ('text',)
+    search_fields = ('title__title',)
+
 
 admin.site.register(Actividad, ActividadAdmin)
--- web/models.py
+++ web/models.py
@@ -1,233 +1,231 @@
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import re
 from django.db import models
 from django.conf import settings
-from datetime import datetime,date
+from datetime import datetime, date
 from copy import copy
 from django.contrib.staticfiles.templatetags.staticfiles import static
 from django.utils.encoding import python_2_unicode_compatible
 from .validators import validate_file_extension
 from tinymce import models as tinymce_models
 
+
 @python_2_unicode_compatible
 class Artista(models.Model):
-	name = models.CharField('Nombre',max_length=70)
+    name = models.CharField('Nombre', max_length=70)
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
-	class Meta:
-		ordering = ['name']
-		verbose_name='Artista'
-		verbose_name_plural='Artistas'
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Artista'
+        verbose_name_plural = 'Artistas'
+
 
 @python_2_unicode_compatible
 class Biografia(models.Model):
-	name = models.OneToOneField(Artista)
-	text = models.TextField('Texto')
-	text_org = models.TextField('Texto')
+    name = models.OneToOneField(Artista)
+    text = tinymce_models.HTMLField()
 
-	def __str__(self):
-		return self.name.name
+    def __str__(self):
+        return self.name.name
 
-	def htmlText(self,txt):
-		texto = u'<p class="text-justify">'
-		texto+= txt.replace(u'\n', u'</p><p class="text-justify">')
-		texto+= u'\r</p>'
-		texto = texto.replace(u'<p class="text-justify">\r</p>',u'')
-		return texto
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Biografia'
+        verbose_name_plural = 'Biografias'
 
-	def save(self, *args, **kwargs):
-		self.text = self.htmlText(self.text_org)
-		super(Biografia, self).save(*args, **kwargs)
-
-	class Meta:
-		ordering = ['name']
-		verbose_name='Biografia'
-		verbose_name_plural='Biografias'
 
 @python_2_unicode_compatible
 class Song(models.Model):
-	songs = models.ForeignKey(Biografia, default=None)
-	name = models.CharField('Nombre',max_length=70)
-	extraInfo = models.TextField('Informacion extra',blank=True)
-	link = models.CharField('Enlace',max_length=300)
-	link_org = models.CharField('Enlace',max_length=300)
+    songs = models.ForeignKey(Biografia, default=None)
+    name = models.CharField('Nombre', max_length=70)
+    extraInfo = models.TextField('Informacion extra', blank=True)
+    link = models.CharField('Enlace', max_length=300)
+    link_org = models.CharField('Enlace', max_length=300)
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
-	def save(self, *args, **kwargs):
-		pattern = re.compile(ur'^\[audio:([\w\/\.]+)\]$', re.UNICODE)
-		url = static(settings.MEDIA_URL+'archive/Biografias/'+re.match(pattern,self.link_org).group(1))
-		self.link = ur'<audio controls><source src="'+url+'" type="audio/mpeg">Su explorador es antiguo\. Actualicelo para reproducir audios\.</audio>'
-		super(Song,self).save(*args, **kwargs)
+    def save(self, *args, **kwargs):
+        pattern = re.compile(ur'^\[audio:([\w\/\.]+)\]$', re.UNICODE)
+        url = static(settings.MEDIA_URL+'archive/Biografias/'+re.match(pattern, self.link_org).group(1))
+        self.link = ur'<audio controls><source src="'+url+'" type="audio/mpeg">Su explorador es antiguo\. Actualicelo para reproducir audios\.</audio>'
+        super(Song, self).save(*args, **kwargs)
 
-	class Meta:	
-		verbose_name='Cancion'
-		verbose_name_plural='Canciones'
+    class Meta:
+        verbose_name = 'Cancion'
+        verbose_name_plural = 'Canciones'
+
+
 @python_2_unicode_compatible
 class Image(models.Model):
-	bio = models.ForeignKey(Biografia, default=None)
-	image = models.ImageField('Imagen',upload_to='images/', default='')
-	description = models.CharField('Descripcion',max_length=200,blank=True)
+    bio = models.ForeignKey(Biografia, default=None)
+    image = models.ImageField('Imagen', upload_to='images/', default='')
+    description = models.CharField('Descripcion', max_length=200, blank=True)
 
-	def __str__(self):
-		return self.description
+    def __str__(self):
+        return self.description
 
-	class Meta:
-		verbose_name='Imagen'
-		verbose_name_plural='Imagenes'
+    class Meta:
+        verbose_name = 'Imagen'
+        verbose_name_plural = 'Imagenes'
+
 
 @python_2_unicode_compatible
 class EfemerideMes(models.Model):
-	month = models.CharField('Mes',max_length=10)
-	monthNumber = models.IntegerField()
-	texto = models.TextField('Efemerides')
-	texto_org = models.TextField('Efemerides')
+    month = models.CharField('Mes', max_length=10)
+    monthNumber = models.IntegerField()
+    texto = models.TextField('Efemerides')
+    texto_org = models.TextField('Efemerides')
 
-	def __str__(self):
-		return self.month
+    def __str__(self):
+        return self.month
 
-	def monthFromString(self,x):
-		return {
-			'Enero': 1,
-			'Febrero': 2,
-			'Marzo': 3,
-			'Abril': 4,
-			'Mayo': 5,
-			'Junio': 6,
-			'Julio': 7,
-			'Agosto': 8,
-			'Septiembre': 9,
-			'Octubre': 10,
-			'Noviembre': 11,
-			'Diciembre': 12
-		}.get(x)
+    def monthFromString(self, x):
+        return {
+            'Enero': 1,
+            'Febrero': 2,
+            'Marzo': 3,
+            'Abril': 4,
+            'Mayo': 5,
+            'Junio': 6,
+            'Julio': 7,
+            'Agosto': 8,
+            'Septiembre': 9,
+            'Octubre': 10,
+            'Noviembre': 11,
+            'Diciembre': 12
+        }.get(x)
 
-	class Meta:
-		verbose_name='Efemeride por mes'
-		verbose_name_plural='Efemerides por mes'
+    class Meta:
+        verbose_name = 'Efemeride por mes'
+        verbose_name_plural = 'Efemerides por mes'
 
-	def save(self,*args,**kwargs):
-		self.monthNumber = self.monthFromString(self.month)
-		self.texto = self.texto_org.replace('\n','').replace('\r','').replace('"','&quot;')
-		super(EfemerideMes,self).save(*args,**kwargs)
-		if (self.efemeride_set.all()!=[]):
-			self.efemeride_set.all().delete()
-		pattern = re.compile(ur"""dia:([0-9]+) *\[(.+?)\]""",flags=re.UNICODE)
-		pattern2 = re.compile(ur"""([0-9]+) *\| *([\W\D .]+)""",flags=re.UNICODE)
-		for (dia, efemeride) in re.findall(pattern,self.texto):
-			for (anio, efe) in re.findall(pattern2,efemeride):
-				efem = Efemeride()
-				efem.date = date(int(anio),self.monthNumber,int(dia))
-				efem.event = efe
-				efem.mes = self
-				efem.save()
+    def save(self, *args, **kwargs):
+        self.monthNumber = self.monthFromString(self.month)
+        self.texto = self.texto_org.replace('\n', '').replace('\r', '').replace('"', '&quot;')
+        super(EfemerideMes, self).save(*args, **kwargs)
+        if (self.efemeride_set.all() != []):
+            self.efemeride_set.all().delete()
+        pattern = re.compile(ur"""dia:([0-9]+) *\[(.+?)\]""", flags=re.UNICODE)
+        pattern2 = re.compile(ur"""([0-9]+) *\| *([\W\D .]+)""", flags=re.UNICODE)
+        for (dia, efemeride) in re.findall(pattern, self.texto):
+            for (anio, efe) in re.findall(pattern2, efemeride):
+                efem = Efemeride()
+                efem.date = date(int(anio), self.monthNumber, int(dia))
+                efem.event = efe
+                efem.mes = self
+                efem.save()
+
 
 @python_2_unicode_compatible
 class Efemeride(models.Model):
-	mes = models.ForeignKey(EfemerideMes)
-	date = models.DateField('Fecha')
-	event = models.CharField('Efemeride',max_length=300)
+    mes = models.ForeignKey(EfemerideMes)
+    date = models.DateField('Fecha')
+    event = models.CharField('Efemeride', max_length=300)
 
-	def __str__(self):
-		return self.date.strftime('Efemeride del %d, %b')
+    def __str__(self):
+        return self.date.strftime('Efemeride del %d, %b')
 
-	class Meta:
-		verbose_name='Efemeride'
-		verbose_name_plural='Efemerides'
+    class Meta:
+        verbose_name = 'Efemeride'
+        verbose_name_plural = 'Efemerides'
+
 
 @python_2_unicode_compatible
 class Discoteca(models.Model):
-	name = models.OneToOneField(Artista)
+    name = models.OneToOneField(Artista)
 
-	def __str__(self):
-		return "Discoteca de " + unicode(self.name)
+    def __str__(self):
+        return "Discoteca de " + unicode(self.name)
+
 
 @python_2_unicode_compatible
 class Album(models.Model):
-	discoteca = models.ForeignKey(Discoteca)
-	name = models.CharField('Nombre', max_length=100)
-	tapa = models.ImageField('Tapa', upload_to='images/', default='', blank=True)
-	lamina = models.ImageField('Lamina', upload_to='images/', default='', blank=True)
-	year = models.IntegerField('Año (Periodo inicio)', null=True,blank=True)
-	yearEnd = models.IntegerField('Año (Periodo final)', null=True,blank=True)
-	songString = models.TextField('Listado de canciones',blank=True)
-	songString_org = models.TextField('Listado de canciones',blank=True)
+    discoteca = models.ForeignKey(Discoteca)
+    name = models.CharField('Nombre', max_length=100)
+    tapa = models.ImageField('Tapa', upload_to='images/', default='', blank=True)
+    lamina = models.ImageField('Lamina', upload_to='images/', default='', blank=True)
+    year = models.IntegerField('Año (Periodo inicio)', null=True, blank=True)
+    yearEnd = models.IntegerField('Año (Periodo final)', null=True, blank=True)
+    songString = models.TextField('Listado de canciones', blank=True)
+    songString_org = models.TextField('Listado de canciones', blank=True)
 
-	def formatHelper(self,matchobj):
-		url = static(settings.MEDIA_URL+'archive/Discografias/'+matchobj.group(1))
-		return ur'<audio controls preload="none"><source src="'+url+'" type="audio/mpeg">Su explorador es antiguo\. Actualicelo para reproducir audios\.</audio>'
-		
-	def save(self,*args,**kwargs):
-		p = re.compile(ur'\[audio:([\w\/\.]+)\]', re.UNICODE)
-		self.songString = re.sub(p,self.formatHelper,self.songString_org)
-		self.songString = self.songString.replace('\n',r'<br>')
-		super(Album,self).save(*args,**kwargs)
+    def formatHelper(self, matchobj):
+        url = static(settings.MEDIA_URL+'archive/Discografias/'+matchobj.group(1))
+        return ur'<audio controls preload="none"><source src="'+url+'" type="audio/mpeg">Su explorador es antiguo\. Actualicelo para reproducir audios\.</audio>'
 
-	class Meta:
-		verbose_name='Album'
-		verbose_name_plural='Albumes'
+    def save(self, *args, **kwargs):
+        p = re.compile(ur'\[audio:([\w\/\.]+)\]', re.UNICODE)
+        self.songString = re.sub(p, self.formatHelper, self.songString_org)
+        self.songString = self.songString.replace('\n', r'<br>')
+        super(Album, self).save(*args, **kwargs)
 
-	def __str__(self):
-		return self.name
+    class Meta:
+        verbose_name = 'Album'
+        verbose_name_plural = 'Albumes'
+
+    def __str__(self):
+        return self.name
+
 
 @python_2_unicode_compatible
 class ImageAlbum(models.Model):
-	alb = models.ForeignKey(Album, default=None)
-	image = models.ImageField('Imagen',upload_to='images/', default='')
-	description = models.CharField('Descripcion',max_length=200,blank=True)
+    alb = models.ForeignKey(Album, default=None)
+    image = models.ImageField('Imagen', upload_to='images/', default='')
+    description = models.CharField('Descripcion', max_length=200, blank=True)
 
-	def __str__(self):
-		return self.description
+    def __str__(self):
+        return self.description
 
-	class Meta:
-		verbose_name='Imagen'
-		verbose_name_plural='Imagenes'
+    class Meta:
+        verbose_name = 'Imagen'
+        verbose_name_plural = 'Imagenes'
+
 
 @python_2_unicode_compatible
 class Actividad(models.Model):
-	name = models.CharField('Nombre', max_length=100)
-	description = tinymce_models.HTMLField()
-	fecha = models.DateField('Fecha', default=date.today)
+    name = models.CharField('Nombre', max_length=100)
+    description = tinymce_models.HTMLField()
+    fecha = models.DateField('Fecha', default=date.today)
 
+    def __str__(self):
+        return self.name
 
-	def __str__(self):
-		return self.name
+    def htmlText(self, txt):
+        texto = u'<p class="text-justify">'
+        texto += txt.replace(u'\n', u'</p><p class="text-justify">')
+        texto += u'\r</p>'
+        texto = texto.replace(u'<p class="text-justify">\r</p>', u'')
+        return texto
 
-	def htmlText(self,txt):
-		texto = u'<p class="text-justify">'
-		texto+= txt.replace(u'\n', u'</p><p class="text-justify">')
-		texto+= u'\r</p>'
-		texto = texto.replace(u'<p class="text-justify">\r</p>',u'')
-		return texto
+    class Meta:
+        verbose_name = 'Actividad'
+        verbose_name_plural = 'Actividades'
 
-	class Meta:
-		verbose_name='Actividad'
-		verbose_name_plural='Actividades'
-	
 
 @python_2_unicode_compatible
 class Video(models.Model):
-	name = models.CharField(max_length=100)
-	videos = models.ForeignKey(Actividad, default=None)
-	link = models.FileField('Video', upload_to='videos/', validators=[validate_file_extension] ,default='')
+    name = models.CharField(max_length=100)
+    videos = models.ForeignKey(Actividad, default=None)
+    link = models.FileField('Video', upload_to='videos/', validators=[validate_file_extension], default='')
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
 
 @python_2_unicode_compatible
 class ActividadImage(models.Model):
-	name = models.CharField(max_length=100)
-	act = models.ForeignKey(Actividad, default=None)
-	link = models.ImageField('Imagen',upload_to='images/', default='')
+    name = models.CharField(max_length=100)
+    act = models.ForeignKey(Actividad, default=None)
+    link = models.ImageField('Imagen', upload_to='images/', default='')
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
-	class Meta:
-		verbose_name='Imagen'
-		verbose_name_plural='Imagenes'
+    class Meta:
+        verbose_name = 'Imagen'
+        verbose_name_plural = 'Imagenes'
