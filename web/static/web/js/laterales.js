04093e09b2bd9e46f1fc958c6eaa894838c22d4f
Manuel Zubieta <mzubieta@onalap1209.corp.onapsis.com>
2020-06-28 11:54:01 -0300

    Fixing laterales in out transitions

--- web/static/web/js/laterales.js
+++ web/static/web/js/laterales.js
@@ -1,5 +1,12 @@
 function loadLaterales() {
 	let images = []
+
+	function clearImages() {
+		let imagesToDelete = document.getElementsByClassName('lateral')
+		for (let index = 0; index < imagesToDelete.length; index++) {
+			imagesToDelete[index].style.visibility = 'hidden'
+		}
+	}
 	
 	function crearImagenes(url) {
 		var newImg = new Image();
@@ -8,14 +15,10 @@ function loadLaterales() {
 		return newImg;
 	}
 
-	function ubicarImagenes(img1, img2, top, lado) {
+	function ubicarImagenes(img1, top, lado) {
 		img1.style.top = top + 'px';
-		img2.style.top = top + 'px';
-		
 		img1.className = `lateral lateral-${lado} visible-lg-block`
-		img2.className = `lateral lateral-${lado} visible-lg-block`
 		$('body>.container').prepend(img1);
-		$('body>.container').prepend(img2);
 	}
 
 	$.ajax({
@@ -23,14 +26,15 @@ function loadLaterales() {
 		type: 'GET',
 	})
 	.done(function(data) {
+		clearImages()
 		$.each(JSON.parse(data), function(_, val) {
 			images.push(crearImagenes(val));
 		});
 		for (var i = 0; i < 3; i++) {
-			ubicarImagenes(images.pop(),images.pop(),200*i,'left')
+			ubicarImagenes(images.pop(),200*i,'left')
 		}
 		for (var i = 0; i < 3; i++) {
-			ubicarImagenes(images.pop(),images.pop(),200*i,'right')
+			ubicarImagenes(images.pop(),200*i,'right')
 		}
 	})	
 	.fail(function(err){
