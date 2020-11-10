4cf9a41f50a7e7e2d79c9d017987f8ee7e374d17
Manuel Zubieta <mzubieta@onalap1209.corp.onapsis.com>
2020-11-10 18:05:43 -0300

    Preventing save audio as with right clicking

--- web/static/web/js/nodownload.js
+++ web/static/web/js/nodownload.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    document.addEventListener('contextmenu', event => event.preventDefault());
     audios = $('audio');
     $.each(audios, function(_, _) {
         $(this).prop("controlsList", "nodownload");
