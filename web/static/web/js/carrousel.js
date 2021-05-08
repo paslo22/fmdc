4107bb52520cdef44b84b4d6508db40d63a5fcb4
manuel zubieta <mzubieta@paypal.com>
2021-05-08 18:30:46 -0300

    Showing slides on page loaded

--- web/static/web/js/carrousel.js
+++ web/static/web/js/carrousel.js
@@ -1,5 +1,4 @@
 var slideIndex = 1;
-showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
@@ -20,9 +19,9 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-//   for (i = 0; i < dots.length; i++) {
-//       dots[i].className = dots[i].className.replace(" active", "");
-//   }
   slides[slideIndex-1].style.display = "block";
-  //dots[slideIndex-1].className += " active";
 }
+
+$(document).ready(function() {
+    showSlides(slideIndex);
+})
